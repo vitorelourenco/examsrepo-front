9ff8e810af863567d35d40ea386727694aee058a
Vitor Lourenco <vitor.registros@lourencos.net>
2021-07-30 22:50:15 -0300

    Refactor create component

--- src/components/Create/Create.js
+++ src/components/Create/Create.js
@@ -1,9 +1,14 @@
 import styled from "styled-components";
 import { getAWSSignature, uploadWithSignature } from "../../utils/aws";
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import Configuration from "./Configuration";
 import ExamBox from "../ExamBox/ExamBox";
+
+const BASE_URL = process.env.REACT_APP_BASE_URL;
+const degreesURL = new URL("degrees", BASE_URL);
+const categoriesURL = new URL("categories", BASE_URL);
+const examsURL = new URL("exams", BASE_URL);
 
 export default function Create() {
   const [file, setFile] = useState(null);
@@ -26,122 +31,36 @@ export default function Create() {
     name: "",
   });
 
-  const BASE_URL = process.env.REACT_APP_BASE_URL;
-  const degreesURL = useMemo(() => new URL("degrees", BASE_URL), [BASE_URL]);
-  const categoriesURL = useMemo(
-    () => new URL("categories", BASE_URL),
-    [BASE_URL]
+  useIntialDegreeOptions(
+    examOptions,
+    setDegrees,
+    setCategories,
+    setExamOptions
   );
-  const examsURL = useMemo(() => new URL("exams", BASE_URL), [BASE_URL]);
 
-  //Get and set the list of degrees and exam categories
-  useEffect(() => {
-    const p1 = axios.get(degreesURL);
+  useOptionsForDegree({
+    examOptions,
+    setDrive,
+    setCourses,
+    setInstructors,
+    setExamOptions,
+  });
 
-    const p2 = axios.get(categoriesURL);
+  useOptionsForCourse({ examOptions, drive, setInstructors, setExamOptions });
 
-    Promise.all([p1, p2])
-      .then(([degreeResponse, categoryResponse]) => {
-        const newExamOptions = { ...examOptions };
-        newExamOptions.degreeId = `${degreeResponse.data[0].id}`;
-        newExamOptions.categoryId = `${categoryResponse.data[0].id}`;
-        setDegrees(degreeResponse.data);
-        setCategories(categoryResponse.data);
-        setExamOptions(newExamOptions);
-      })
-      .catch((err) => alert(err));
-  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+  useSubmitEnabler({
+    file,
+    link,
+    submitType,
+    examOptions,
+    isSubmitDisabled,
+    setIsSubmitDisabled,
+  });
 
-  //recalculate the courses and instructors options when the degree changes
-  useEffect(() => {
-    if (!examOptions.degreeId) return;
-    const driveURL = new URL(`degrees/drive/${examOptions.degreeId}`, BASE_URL);
-    axios.get(driveURL).then(({ data }) => {
-      setDrive(data);
-      const courses = data?.courses || [];
-      const prunedCourses =
-        courses?.map((course) => ({ id: course.id, name: course.name })) || [];
-      setCourses(prunedCourses);
-
-      const instructors = courses[0]?.instructors || [];
-      const prunedInstructors =
-        instructors?.map((instructors) => ({
-          id: instructors.id,
-          name: instructors.name,
-        })) || [];
-      setInstructors(prunedInstructors);
-
-      const newExamOptions = { ...examOptions };
-      newExamOptions.courseId = `${courses[0]?.id || ""}`;
-      newExamOptions.instructorId = `${instructors[0]?.id || ""}`;
-      setExamOptions(newExamOptions);
-    });
-  }, [examOptions.degreeId, BASE_URL]); // eslint-disable-line react-hooks/exhaustive-deps
-
-  //recalculate the instructor options when course changes
-  useEffect(() => {
-    if (!examOptions.courseId) return;
-    const course = drive.courses.find(
-      (course) => `${course.id}` === examOptions.courseId
-    );
-    setInstructors(course?.instructors || []);
-    const newExamOptions = { ...examOptions };
-    newExamOptions.instructorId = `${course?.instructors[0]?.id || ""}`;
-    setExamOptions(newExamOptions);
-  }, [examOptions.courseId, drive.courses]); // eslint-disable-line react-hooks/exhaustive-deps
-
-  //check if the form is ready to submit
-  useEffect(() => {
-    const shouldStayDisabled = (() => {
-      if (!file && submitType === "upload") return true;
-      if (!link && submitType === "link") return true;
-      if (!examOptions.courseId) return true;
-      if (!examOptions.name) return true;
-      if (!examOptions.instructorId) return true;
-      if (!examOptions.degreeId) return true;
-      if (!examOptions.categoryId) return true;
-      return false;
-    })();
-    if (shouldStayDisabled !== isSubmitDisabled) {
-      setIsSubmitDisabled(!isSubmitDisabled);
-    }
-  },[submitType,file,link,examOptions.courseId,examOptions.name,examOptions.instructorId,examOptions.degreeId,examOptions.categoryId]); // eslint-disable-line react-hooks/exhaustive-deps
-
-  const onFormSubmit = (e) => {
-    e.preventDefault();
-
-    const courseId = parseInt(examOptions.courseId);
-    const instructorId = parseInt(examOptions.instructorId);
-    const degreeId = parseInt(examOptions.degreeId);
-    const categoryId = parseInt(examOptions.categoryId);
-    const name = examOptions.name;
-
-    const body = { courseId, instructorId, degreeId, categoryId, name, fileLink: link };
-
-    if (submitType === "upload") {
-      getAWSSignature(file)
-      .then(({data})=>{
-        const blob = file.slice(0, file.size, file.type); 
-        const newFile = new File([blob], data.newFileName, {type: file.type});
-        return uploadWithSignature(newFile, data.awsData.signedRequest, data.awsData.url);
-      })
-      .then(({data})=>{
-        body.fileLink = data.url;
-        return axios.post(examsURL, body)
-      })
-      .then(()=>alert("success"))
-      .catch((err)=>alert(err));
-    } else {
-      axios
-        .post(examsURL, body)
-        .then(() => alert("sucesso"))
-        .catch((err) => alert(err));
-    }
-    
-  };
+  const handleSubmit = (e) => onFormSubmit(e,examOptions,file,link,submitType)
 
   return (
-    <Form onSubmit={onFormSubmit}>
+    <Form onSubmit={handleSubmit}>
       <Configuration
         {...{
           examOptions,
@@ -173,3 +92,159 @@ const Form = styled.form`
   display: flex;
   justify-content: space-between;
 `;
+
+function onFormSubmit (e,examOptions,file,link,submitType) {
+  e.preventDefault();
+
+  const courseId = parseInt(examOptions.courseId);
+  const instructorId = parseInt(examOptions.instructorId);
+  const degreeId = parseInt(examOptions.degreeId);
+  const categoryId = parseInt(examOptions.categoryId);
+  const name = examOptions.name;
+
+  const body = {
+    courseId,
+    instructorId,
+    degreeId,
+    categoryId,
+    name,
+    fileLink: link,
+  };
+
+  if (submitType === "upload") {
+    getAWSSignature(file)
+      .then(({ data }) => {
+        const blob = file.slice(0, file.size, file.type);
+        const newFile = new File([blob], data.newFileName, {
+          type: file.type,
+        });
+        return uploadWithSignature(
+          newFile,
+          data.awsData.signedRequest,
+          data.awsData.url
+        );
+      })
+      .then(({ data }) => {
+        body.fileLink = data.url;
+        return axios.post(examsURL, body);
+      })
+      .then(() => alert("success"))
+      .catch((err) => alert(err));
+  } else {
+    axios
+      .post(examsURL, body)
+      .then(() => alert("sucesso"))
+      .catch((err) => alert(err));
+  }
+};
+
+
+function useSubmitEnabler({
+  file,
+  link,
+  submitType,
+  examOptions,
+  isSubmitDisabled,
+  setIsSubmitDisabled,
+}) {
+  //check if the form is ready to submit
+  useEffect(() => {
+    const shouldStayDisabled = (() => {
+      if (!file && submitType === "upload") return true;
+      if (!link && submitType === "link") return true;
+      if (!examOptions.courseId) return true;
+      if (!examOptions.name) return true;
+      if (!examOptions.instructorId) return true;
+      if (!examOptions.degreeId) return true;
+      if (!examOptions.categoryId) return true;
+      return false;
+    })();
+    if (shouldStayDisabled !== isSubmitDisabled) {
+      setIsSubmitDisabled(!isSubmitDisabled);
+    }
+  }, [// eslint-disable-line react-hooks/exhaustive-deps
+    submitType,
+    file,
+    link,
+    examOptions.courseId,
+    examOptions.name,
+    examOptions.instructorId,
+    examOptions.degreeId,
+    examOptions.categoryId,
+  ]);
+}
+
+function useIntialDegreeOptions(
+  examOptions,
+  setDegrees,
+  setCategories,
+  setExamOptions
+) {
+  useEffect(() => {
+    const p1 = axios.get(degreesURL);
+
+    const p2 = axios.get(categoriesURL);
+
+    Promise.all([p1, p2])
+      .then(([degreeResponse, categoryResponse]) => {
+        const newExamOptions = { ...examOptions };
+        newExamOptions.degreeId = `${degreeResponse.data[0].id}`;
+        newExamOptions.categoryId = `${categoryResponse.data[0].id}`;
+        setDegrees(degreeResponse.data);
+        setCategories(categoryResponse.data);
+        setExamOptions(newExamOptions);
+      })
+      .catch((err) => alert(err));
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+}
+
+function useOptionsForDegree({
+  examOptions,
+  setDrive,
+  setCourses,
+  setInstructors,
+  setExamOptions,
+}) {
+  useEffect(() => {
+    if (!examOptions.degreeId) return;
+    const driveURL = new URL(`degrees/drive/${examOptions.degreeId}`, BASE_URL);
+    axios.get(driveURL).then(({ data }) => {
+      setDrive(data);
+      const courses = data?.courses || [];
+      const prunedCourses =
+        courses?.map((course) => ({ id: course.id, name: course.name })) || [];
+      setCourses(prunedCourses);
+
+      const instructors = courses[0]?.instructors || [];
+      const prunedInstructors =
+        instructors?.map((instructors) => ({
+          id: instructors.id,
+          name: instructors.name,
+        })) || [];
+      setInstructors(prunedInstructors);
+
+      const newExamOptions = { ...examOptions };
+      newExamOptions.courseId = `${courses[0]?.id || ""}`;
+      newExamOptions.instructorId = `${instructors[0]?.id || ""}`;
+      setExamOptions(newExamOptions);
+    });
+  }, [examOptions.degreeId, BASE_URL]); // eslint-disable-line react-hooks/exhaustive-deps
+}
+
+function useOptionsForCourse({
+  examOptions,
+  drive,
+  setInstructors,
+  setExamOptions,
+}) {
+  useEffect(() => {
+    if (!examOptions.courseId) return;
+    const course = drive.courses.find(
+      (course) => `${course.id}` === examOptions.courseId
+    );
+    setInstructors(course?.instructors || []);
+    const newExamOptions = { ...examOptions };
+    newExamOptions.instructorId = `${course?.instructors[0]?.id || ""}`;
+    setExamOptions(newExamOptions);
+  }, [examOptions.courseId, drive.courses]); // eslint-disable-line react-hooks/exhaustive-deps
+}
